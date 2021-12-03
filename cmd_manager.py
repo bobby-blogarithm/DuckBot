76652b060c131ddb3e128dd310065c83475c8edf
Joshua O'Callaghan <jcallaghan1250@asu.edu>
2021-12-03 16:00:10 -0700

    duck_api adds images and facts for all your duck needs

--- cmd_manager.py
+++ cmd_manager.py
@@ -4,6 +4,9 @@ import os
 import sys
 import helpers
 import discord
+import duck_api
+import io
+import aiohttp
 
 from economy import Economy
 
@@ -37,6 +40,21 @@ class CommandManager(disc_cmds.Cog, name='CommandManager'):
         leaderboard_embed.add_field(name='Names', value=name_string if name_string else 'N/A')
         leaderboard_embed.add_field(name='Points', value=point_string if point_string else 'N/A')
         await ctx.send(embed=leaderboard_embed)
+
+    @disc_cmds.command(name='duckfact')
+    async def duck_fact(self, ctx):
+        duck = duck_api.DuckFact()
+        image = duck.getImage()
+        fact = duck.getFact()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image) as resp:
+                if resp.status != 200:
+                    return await ctx.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await ctx.send(file=discord.File(data, 'duck.png'), content=fact)
+
+
 
     # TODO This command should be used for the bot to update itself
     # ! DEPRECATED for now
