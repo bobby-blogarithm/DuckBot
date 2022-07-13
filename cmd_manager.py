c90a321d031b575ac77eb29b6ef5eaf0d4c94585
Xuanli Lin <l19980623@gmail.com>
2022-07-13 12:51:24 -0700

    removed the test thingy that was accidentally left in

--- cmd_manager.py
+++ cmd_manager.py
@@ -183,44 +183,6 @@ class CommandManager(disc_cmds.Cog, name='CommandManager'):
         await asyncio.sleep(sec_duration)
         await ctx.send(content=f'<@{ctx.author.id}> The {padded_name}timer is up!', delete_after=300.0)
 
-    @disc_cmds.command(name='test')
-    async def pages(self, ctx):
-        contents = ["This is page 1!", "This is page 2!", "This is page 3!", "This is page 4!"]
-        pages = 4
-        cur_page = 1
-        message = await ctx.send(f"Page {cur_page}/{pages}:\n{contents[cur_page - 1]}")
-
-        # getting the message object for editing and reacting
-
-        def check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
-            # This makes sure nobody except the command sender can interact with the "menu"
-
-        while True:
-            try:
-                reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
-                # waiting for a reaction to be added - times out after x seconds, 60 in this
-                # example
-
-                if str(reaction.emoji) == "▶️" and cur_page != pages:
-                    cur_page += 1
-                    await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page - 1]}")
-                    await message.remove_reaction(reaction, user)
-
-                elif str(reaction.emoji) == "◀️" and cur_page > 1:
-                    cur_page -= 1
-                    await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page - 1]}")
-                    await message.remove_reaction(reaction, user)
-
-                else:
-                    await message.remove_reaction(reaction, user)
-                    # removes reactions if the user tries to go forward on the last page or
-                    # backwards on the first page
-            except asyncio.TimeoutError:
-                await message.delete()
-                break
-                # ending the loop if user doesn't react after x seconds
-
     # TODO This command should be used for the bot to update itself
     # ! DEPRECATED for now
     # @disc_cmds.command(name='update')
